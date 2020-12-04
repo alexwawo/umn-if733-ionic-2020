85aa366d28fd96c132f4edfa984040b3cc06bd93
Alexander Waworuntu <alex.wawo@gmail.com>
2020-12-04 11:59:11 +0700

    php source code

---
+++ php-if733-2020/insert.php
@@ -0,0 +1,31 @@
+<?php
+if(isset($_SERVER['HTTP_ORIGIN'])){
+	header('Access-Control-Allow-Origin: *');
+	header('Access-Control-Allow-Methods: POST, PUT, DELETE, OPTIONS');
+	header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
+}
+
+$koneksi = mysqli_connect('127.0.0.1', 'root', '', 'umn_if733_2020');
+if($koneksi->connect_error){
+	die("Koneksi gagal: " . $koneksi->connect_error);
+}else{
+	$input = file_get_contents('php://input');
+	$data = json_decode($input, true);
+	$message = array();
+
+	$nim = $data['nim'];
+	$nama = $data['nama'];
+	$prodi = $data['prodi'];
+
+	$sql = "INSERT INTO `mahasiswa` (`nim`, `nama`, `prodi`) VALUES ('{$nim}', '{$nama}', '{$prodi}')";
+	$q = $koneksi->query($sql);
+
+	if($q){
+		$message['status'] = "success";
+	}else{
+		$message['status'] = mysqli_error($koneksi);
+	}
+
+	mysqli_close();
+	echo json_encode($message);
+}
---
+++ php-if733-2020/select1.php
@@ -0,0 +1,27 @@
+<?php
+if(isset($_SERVER['HTTP_ORIGIN'])){
+	header('Access-Control-Allow-Origin: *');
+	header('Access-Control-Allow-Methods: POST, PUT, DELETE, OPTIONS');
+	header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
+}
+
+$koneksi = mysqli_connect('127.0.0.1', 'root', '', 'umn_if733_2020');
+if($koneksi->connect_error){
+	die("Koneksi gagal: " . $koneksi->connect_error);
+}else{
+	$nim = $_GET['nim'];
+	$sql = "SELECT * FROM mahasiswa WHERE nim='$nim'";
+	$hasil = $koneksi->query($sql);
+
+	$mahasiswa = array();
+	if($hasil->num_rows > 0){
+		while($row = $hasil->fetch_assoc()){
+			array_push($mahasiswa, $row);
+		}
+		echo json_encode($mahasiswa);
+	}else{
+		echo "0 results";
+	}
+
+	$koneksi->close();
+}
